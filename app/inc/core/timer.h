334f5cf45895e672b075e9cce2349a628705533f
vaidaa1 <vaidaa1@Xubuntu.myguest.virtualbox.org>
2024-10-15 13:54:37 +0530

    1. Added frequency notes and delays arrays for super mario bros 1-bit music tone and made changes to set_duty_ratio function so it also changes frequency, by changing the ARR value 2. Not able to produce the full tone, it gets stuck at parts and doesnt continue forward 3. tone only heard in the first while-loop iteration, not in the following ones 4. hmmmmmmm

--- app/inc/core/timer.h
+++ app/inc/core/timer.h
@@ -4,6 +4,6 @@
 #include "common-defines.h"
 
 void timer_setup(void);
-void timer_pwm_set_duty_ratio(float duty_ratio);    // 0 < duty_cycle < 1
+void timer_pwm_set_duty_ratio_and_frequency(float duty_ratio, uint16_t freq_val);    // 0 < duty_cycle < 1
 
 #endif // INC_TIMER_H
--- app/src/firmware.c
+++ app/src/firmware.c
@@ -25,23 +25,54 @@ int main(void)
   gpio_setup();
   timer_setup();
   
-  uint32_t start_time = system_get_ticks();
-  volatile float duty_ratio = 0.0f;
+  // super mario bros (overworld) frequency notes and following delays
+  uint16_t freqs[] = {660, 660, 660, 510, 660, 770,
+                      380, 510, 380, 320, 440, 480,
+                      450, 430, 380, 660, 760, 860, 
+                      700, 760, 660, 520, 580, 480};
 
-  timer_pwm_set_duty_ratio(duty_ratio);
+  uint16_t delay_dur[] = {150, 300, 300, 100, 300, 550,
+                          575, 450, 400, 500, 300, 330, 
+                          150, 300, 200, 200, 150, 300, 
+                          150, 350, 300, 150, 150, 500};
+
+  uint32_t start_time = system_get_ticks();
+  float duty_ratio = 0.0f;
+
+  timer_pwm_set_duty_ratio_and_frequency(duty_ratio, freqs[0]);
       
   while(1)
   {
-    if(system_get_ticks() - start_time >= 10)
+    // if(system_get_ticks() - start_time >= 10)
+    // {
+    //   duty_ratio += 1.0f;
+    //   if (duty_ratio > 100.0f)
+    //   {
+    //     duty_ratio = 0.0f;
+    //   }
+    //   timer_pwm_set_duty_ratio(duty_ratio);
+    //   start_time = system_get_ticks();
+    // }
+
+    for (size_t i = 0; i < sizeof(freqs)/sizeof(uint16_t); i++)
     {
-      duty_ratio += 1.0f;
-      if (duty_ratio > 100.0f)
-      {
-        duty_ratio = 0.0f;
-      }
-      timer_pwm_set_duty_ratio(duty_ratio);
       start_time = system_get_ticks();
-     }
+      duty_ratio = 50.0f;
+      timer_pwm_set_duty_ratio_and_frequency(duty_ratio, freqs[i]);
+      while (system_get_ticks() - start_time < 100)
+      {
+        /* spin */
+      }
+
+      start_time = system_get_ticks(); 
+      duty_ratio = 0.0f;
+      timer_pwm_set_duty_ratio_and_frequency(duty_ratio, freqs[i]);  
+      while (system_get_ticks() - start_time < (delay_dur[i] - 100))
+      {
+        /* spin */
+      }
+    }
+     
   }
         
 
